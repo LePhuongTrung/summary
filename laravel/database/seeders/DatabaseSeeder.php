1048e8fb065df914db64eb56c1c8d82c67086257
LePhuongTrung <trunglp2001@gmail.com>
2024-08-06 11:46:41 +0700

    update seed for eloquent user

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Infrastructure\Eloquent\EloquentUser;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,9 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        EloquentUser::factory(10)->create();
 
-        User::factory()->create([
+        EloquentUser::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
